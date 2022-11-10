1928ae1edf66685a04abf7b6f2017c1e5633db4f
Charanjeet Singh <charanjeets036@gmail.com>
2022-11-10 10:09:21 -0800

    theme initiliazed

---
+++ wp-content/themes/cetechfolio/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Portfolio-Charanjeet Singh</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <!-- CSS Libraries -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/fontawesome/css/all.min.css" rel="stylesheet">
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+    <?php wp_head(); ?>
+
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="51">
+<?php wp_body_open(); ?>
+
+<!-- Nav Bar Start -->
+<div class="navbar navbar-expand-lg bg-light navbar-light">
+    <div class="container-fluid">
+        <a href="index.html" class="navbar-brand"><img src="./img/logo.png" alt="" class="logo"></a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+            <div class="navbar-nav ml-auto">
+                <a href="#home" class="nav-item nav-link active">Home</a>
+                <a href="#about" class="nav-item nav-link">About</a>
+                <a href="#service" class="nav-item nav-link">Service</a>
+                <a href="#experience" class="nav-item nav-link">Experience</a>
+                <a href="#contact" class="nav-item nav-link">Contact</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Nav Bar End -->
